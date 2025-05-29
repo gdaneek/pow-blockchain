6955cdac4bf1ce74c9b9b033d5b96ed0294b8b4e
gdaneek <daniladgubazov@gmail.com>
2025-05-29 23:41:35 +0300

    [upgrade] aux added; README updated

---
+++ README.md
@@ -0,0 +1,30 @@
+# Implementation of the simplest PoW algorithm
+
+### Installation
+
+clone the project with all the submodules:
+
+```
+git clone --recurse-submodules https://github.com/gdaneek/pow-blockchain.git
+```
+
+After run the build with cmake:
+```
+mkdir build
+cd build
+cmake ..
+cmake --build .
+```
+As a result, you will receive a "pow" executable file containing an example of the PoW algorithm.
+
+### Documentation
+
+See docs/code or generate it yourself using doxygen:
+```
+cd docs
+doxygen
+```
+
+### Related projects
+
+See external/ subfolder to get a list of submodules
---
+++ aux/txn_gen.cc
@@ -0,0 +1,40 @@
+ /**
+ *  @file    txn_gen.cc
+ *  @brief   random transaction generator for TestBlockchain
+ *  @author  https://github.com/gdaneek
+ *  @date    30.05.2025
+ *  @version 1.0
+ */
+#define STREEBOG_ENABLE_WRAPPERS
+#include "streebog.hh"
+#include "prng.hpp"
+
+#include <fstream>
+#include <array>
+
+struct Hasher {
+    using value_type = typename std::array<uint64_t, 4>;
+    auto operator()(auto&& x) const -> value_type {
+        return streebog256(x.data(), x.size() * sizeof(*x.data()));
+    }
+};
+
+
+int main(int argc, char* argv[]) {
+    auto bytes_n = std::stoll(argv[argc-1]);
+    auto seed = "danila gubazov";
+
+    HashRNG<Hasher> rng(seed);
+
+    for(size_t i{};i < argc - 2;i++) {
+        std::ofstream fout(argv[i+1], std::ios::binary);
+        auto x = rng.next();
+
+        for(size_t bw{};bw + sizeof(x) < bytes_n; bw += sizeof(x)) {
+            fout.write((const char*)x.data(), sizeof(x));
+            x = rng.next();
+        }
+    }
+
+    return 0;
+}
